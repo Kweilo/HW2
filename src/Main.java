125a8837cb90e9748bd483f83d938a25798235f4
Kweilo <kweilo.yandere@mail.ru>
2024-10-20 00:00:22 +0300

    Задания выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // Задание 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задание 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задание 4
+        var friend = 19;
+        friend = (friend + 2) / 7;
+        System.out.println(friend);
+
+        //Задание 5
+        var frog = 3.5;
+        frog = (frog * 10) / 3.5 + 4;
+        System.out.println(frog);
+
+        //Задание 6
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var result = boxer1 + boxer2;
+        System.out.println("boxer1 + boxer2 = " + result);
+        var difference = boxer2 - boxer1;
+        System.out.println("boxer1 - boxer2 = " + difference);
+
+        //Задание 7
+        var modulo = boxer2 % boxer1;
+        System.out.println("boxer1 % boxer2 = " + modulo);
+
+        //Задание 8
+        var time = 640;
+        var worktime = 8;
+        var workers = time / worktime;
+        System.out.println("Всего работников в компании — " + workers + " человек");
+
+        var workers2 = workers + 94;
+        var hours = time / workers2;
+        System.out.println("Если в компании работает " + workers2 + " человек, то всего "+ hours + " часов работы может быть поделено между сотрудниками");
+
     }
 }
